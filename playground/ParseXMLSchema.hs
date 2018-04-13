05b7e08e9d6280ac2e0e6994fc4824a0416f1114
Kevin Cheung <mathapptician@gmail.com>
2018-04-13 05:15:58 -0400

    Need to fix the sequence extraction and element extraction functions. Must pay attention to matching closing tags, not just closing tags.

--- playground/ParseXMLSchema.hs
+++ playground/ParseXMLSchema.hs
@@ -15,6 +15,7 @@ fromXmlFlatType s = case s of
   "xs:double" -> "Double"
   otherwise -> "String"
 
+simpleType :: Tag String -> (String, String)
 simpleType e = 
     (typeName, fromXmlFlatType flat)
    where
@@ -25,37 +26,61 @@ dropUntilOpenTag str = dropWhile (not . (isTagOpenName str))
 
 takeUntilCloseTag str = takeWhile (not . (isTagCloseName str))
 
-extractSeq tags =
-  let str = "xs:sequence"
-      s = takeUntilCloseTag str $ tail $ dropUntilOpenTag str tags in
-  filter (isTagOpenName "xs:element") s
+-- This doesn't work when there is a nested elements.
+-- Need to take until MATCHING closing tag.
+extractElt [] = []
+extractElt tags =
+  let
+    firstElt = 
+      if (isSimpleType (head tags)) then
+        [head tags]
+      else
+        takeUntilCloseTag "xs:element" tags in
+  firstElt:(extractElt (dropUntilOpenTag "xs:element" $ tail tags)) 
 
-complexType typeName c = 
-  let openCpxTag = head c
+-- This doesn't work when there is a nested sequence.
+-- Need to take until MATCHING closing tag
+extractSeq :: [ Tag String ] -> [ [Tag String] ]
+extractSeq tags =
+  let 
+    str = "xs:sequence"
+    seq = takeUntilCloseTag str $ tail $ dropUntilOpenTag str tags in
+  extractElt $ dropUntilOpenTag "xs:element" seq
+
+genTypesHelper :: [Tag String] -> (String, String, String, Bool)
+genTypesHelper tags = 
+  if (isSimpleType openEltTag) then
+    let
+      typeName = capitalize eltName
+      typeStr = fromXmlFlatType $ fromAttrib "type" openEltTag in
+    ("newtype " ++ typeName ++ " = " ++ typeStr, eltName, typeStr, False)
+  else
+    let
+      c = dropWhile (not . (isTagOpenName "xs:complexType")) $ tail t
+      openCpxTag = head c
       isMixed = (fromAttrib "mixed" openCpxTag) == "true" in
-  case isMixed of
-    True -> "Mixed type.  Don't know how to do this yet"
-    False -> -- Just a record type
-      let elements = extractSeq $ tail c
-          nameTypePairs = map simpleType elements
-          typeStrs = map (\(t,n) -> t ++ " :: " ++ n) nameTypePairs
-          depTypes = concat $ intersperse "\n  , " typeStrs in
-      "data " ++ typeName ++ " = " ++ typeName ++ " {\n    " 
-              ++ depTypes
-              ++ "\n}\n"
-      
+    if (isMixed) then
+      ("Mixed type.  Don't know how to do this yet", "null", "null", True)
+    else
+      let
+        elements = extractSeq $ tail c
+        typeStrTriples = map genTypesHelper elements
+        complexTypeStrs = filter (\(_,_,_, isComplex) -> isComplex) typeStrTriples
+        depTypeDefStr = intercalate "\n\n" $ map (\(t,_,_,_) -> t) complexTypeStrs
+        innerTypeStrs = map (\(_,elt,typ,_) -> elt ++ " :: " ++ typ) typeStrTriples
+        depTypes = intercalate "\n  , " innerTypeStrs in
+      (depTypeDefStr ++ "\n\ndata " ++ typeName ++ " = " ++ typeName ++ " {\n    " 
+        ++ depTypes ++ "\n}\n" , eltName, typeName, True)
+  where
+    openEltTag = head tags
+    eltName = fromAttrib "name" openEltTag
+    t = tail tags
+    typeName = capitalize $ fromAttrib "name" openEltTag
 
 genTypes::String -> String
 genTypes s = 
-    let t = dropWhile (not . (isTagOpenName "xs:element")) tags 
-        openEltTag = head t
-        typeName = capitalize $ fromAttrib "name" openEltTag in
-    case isSimpleType openEltTag of
-      True -> let (n, typeStr) = simpleType openEltTag in
-              "newtype " ++ (capitalize n) ++ " = " ++ typeStr
-      False -> complexType typeName $ dropWhile (not . (isTagOpenName "xs:complexType")) $ tail t
-  where
-    tags = parseTags s
+  let (str,_,_,_) = genTypesHelper $ dropWhile (not . (isTagOpenName "xs:element")) $ parseTags s in
+  str
 
 
 simpleXML = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<xs:schema xmlns:xs=\"http://www.w3.org/2001/XMLSchema\">\n<xs:element name=\"shiporder\" type=\"xs:float\">\n</xs:element>\n</xs:schema>"
@@ -67,5 +92,5 @@ simple = genTypes simpleXML
 complex = genTypes personXML
 
 main = do
-   putStrLn simple
-   putStrLn complex
+  xml <- readFile "person.xsd"
+  putStrLn $ genTypes xml
