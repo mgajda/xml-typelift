ee7b300815ac74f11f3ad72f77968296310ba2d5
Michal J. Gajda <mjgajda@migamake.com>
2018-07-22 17:16:05 +0200

    Importing type by name.

--- src/States.hs
+++ src/States.hs
@@ -8,13 +8,17 @@ module States where
 import Prelude hiding (id)
 
 import System.IO(stderr)
+import Data.Char(ord)
+import Data.Maybe(fromMaybe)
 import Data.Monoid
 import Data.ByteString.Char8 as BS
+import qualified Data.Map as Map
 import Data.Set
 import Control.Monad.State.Strict as St
 import Control.Monad.IO.Class
 import GHC.Generics
 import System.Exit(exitFailure)
+import Data.ByteString.Internal(ByteString(..))
 
 import Xeno.SAX as Xeno
 
@@ -23,7 +27,7 @@ import Schema
 -- | Keep partly built structure to be merged upwards when the element is closed.
 data Builder = BContent Content
              | BElement Element
-             | BType    Type
+             | BType    { tName :: XMLString, bType :: Type}
              | BAttr    Attr
              | BSchema  Schema
   deriving (Eq, Ord, Show, Generic)
@@ -61,18 +65,40 @@ openTagE s@[BSchema _] (stripNS -> "schema")  = s
 openTagE bs t@(stripNS -> "schema") = error $ BS.unpack $ "Nested schema element:" <> bshow t
                                            <> "state:" <> bshow bs
 openTagE bs (stripNS -> "element"       ) = BElement def:bs
-openTagE bs (stripNS -> "simpleType"    ) = BType    def:bs
-openTagE bs (stripNS -> "complexType"   ) = BType    def:bs
+openTagE bs (stripNS -> "simpleType"    ) = BType    "" (Complex [] def):bs
+openTagE bs (stripNS -> "complexType"   ) = BType    "" (Complex [] def):bs
 openTagE bs (stripNS -> "attribute"     ) = BAttr    def:bs
-openTagE bs (stripNS -> "simpleContent" ) = BType    (Restriction "any" None):bs
-openTagE bs (stripNS -> "complexContent") = BType    (Complex [] def):bs
+openTagE bs (stripNS -> "simpleContent" ) = BType    "" (Complex [] def):bs
+openTagE bs (stripNS -> "complexContent") = BType    "" (Complex [] def)        :bs
 openTagE bs (stripNS -> "sequence"      ) = BContent (Seq     []    ):bs
 openTagE bs (stripNS -> "choice"        ) = BContent (Choice  []    ):bs
 openTagE bs  _                            = bs
 
 attrE :: PState -> XMLString -> XMLString -> PState
-attrE       bs (stripNS -> "name") v = bs
-attrE       bs  _                  v = bs
+attrE (b:bs) (stripNS -> "name") v = setName b v:bs
+attrE    bs  _                   v = bs
+
+setName :: Builder -> XMLString -> Builder
+setName   (BElement   e) n = BElement $ e { name =n }
+setName   (BAttr      a) n = BAttr    $ a { aName =n }
+setName c@(BType     {}) n =            c { tName=n }
+setName   (BContent   _) n = error $ "Attribute _name_ not allowed in content: " <> BS.unpack n
+
+parseError (PS ptrInput iStart iEnd)
+           (PS ptrErr start end) msg | ptrInput == ptrErr
+                                    && iStart <= start
+                                    && iEnd   >= end =
+    error $ msg <> " in line " <> show lineCount
+         <>        " at "      <> show start          <> ":\n"
+         <> BS.unpack (PS ptrInput wrapStart wrapEnd) <> "\n"
+         <> [' ' | _ <- [0..start-wrapStart]]         <> "^"
+  where
+    lineCount     = BS.count '\n' (PS ptrInput iStart start)
+    wrapStart     = max iStart   lastLineStart
+    wrapEnd       = min iEnd   $ end+40
+    lastLineStart = (start-10) `fromMaybe` BS.elemIndexEnd '\n' tilError
+    tilError      = PS ptrInput (max iStart $ start-40) start
+
 
 endOpenTagE  s   t   = s-- here we can validate
 closeTagE   s t | stripNS t `Prelude.notElem` handledTags = s
@@ -82,8 +108,13 @@ closeTagE   (BElement e:BContent c:s) (stripNS -> "element") =
   (BContent $ contentAppend c e):s
 closeTagE   [BElement e, BSchema s]   (stripNS -> "element") =
   [BSchema $ s { tops = e:tops s }]
-closeTagE   (BAttr a:BType (Complex {subs, attrs}):bs) (stripNS -> "attribute") =
-   BType (Complex { subs, attrs = a:attrs }):bs
+closeTagE   [BType tName ty,BSchema s@Schema {types}]
+                                      (stripNS -> "complexType") =
+  [BSchema $ s { types = Map.insert tName ty types}]
+closeTagE   (BAttr a:BType {tName, bType=Complex {subs, attrs}}:bs) (stripNS -> "attribute") =
+   BType {tName, bType=Complex { subs, attrs = a:attrs }}:bs
+closeTagE   (BAttr a:bTy@BType {}:bs) (stripNS -> "attribute") =
+   error $ "Attribute within non-complex type:" <> show bTy
 {-closeTagE   (BElement e:bs) tag = error $ "Expected </element>, got: " <> BS.unpack tag
                                        <> "inside:" <> show bs-}
 closeTagE   (_:b:bs) t | stripNS t `Prelude.elem` handledTags &&
