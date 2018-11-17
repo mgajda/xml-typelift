5df2473de5f0f0ef457babd5da3ce687f405d619
Michal J. Gajda <migamake@migamake.com>
2018-11-17 22:40:14 +0100

    Cleanup.

--- lib/FromXML.hs
+++ lib/FromXML.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE ViewPatterns      #-}
 module FromXML(FromXML(..),
                makeFromXML,
-               DecodingError(..),
                Result,
                AttrHandler,
                ChildHandler,
@@ -15,7 +14,6 @@ module FromXML(FromXML(..),
                splitNS,
                stripNS,
                bshow,
-               revTake,
                printExceptions,
                displayException
               ) where
@@ -26,21 +24,18 @@ import           Data.ByteString.Internal(ByteString(..))
 import           System.IO(stderr)
 import           Xeno.Types as Xeno
 import           Xeno.DOM as Xeno
+import           Xeno.Errors
 
 type XenoAttribute = (ByteString, ByteString)
-
-data DecodingError = DecodingError { errorIndex   :: {-# UNPACK #-} !Int
-                                   , errorMessage :: {-# UNPACK #-} !BS.ByteString }
-  deriving (Show, Eq)
 
 type Result elt = Either XenoException elt
 
 type AttrHandler  elt = elt -> XenoAttribute -> Either XenoException elt
 type ChildHandler elt = elt -> Node          -> Either XenoException elt
 
-{-# INLINE CONLIKE failHere #-}
-failHere :: BS.ByteString -> BS.ByteString -> Either XenoException a
-failHere msg here = Left $ XenoParseError (getStartIndex here) msg
+-- | Show for ByteStrings
+bshow :: Show a => a -> BS.ByteString
+bshow = BS.pack . show
 
 -- Attribute handler for a given type of element
 class FromXML elt where
@@ -64,10 +59,6 @@ makeFromXML (attrHandler, childHandler) pristine aNode = do
   withAttrs <- foldM attrHandler pristine               (Xeno.attributes aNode)
   foldM                          childHandler withAttrs (Xeno.children   aNode)
 
-{-# INLINE CONLIKE getStartIndex #-}
-getStartIndex :: BS.ByteString -> Int
-getStartIndex (PS _ from _) = from
-
 {-# NOINLINE unknownAttrHandler #-}
 unknownAttrHandler :: BS.ByteString -> XenoAttribute -> Result elt
 unknownAttrHandler desc (splitNS -> (ns, aName), aVal) =
@@ -75,9 +66,6 @@ unknownAttrHandler desc (splitNS -> (ns, aName), aVal) =
   <> " in namespace '" <> ns
   <> "' : '" <> aName <> "' = '" <> aVal <> "'")
         `failHere` aName
-
-bshow :: Show a => a -> BS.ByteString
-bshow = BS.pack . show
 
 {-# INLINABLE splitNS #-}
 splitNS :: ByteString -> (ByteString, ByteString)
@@ -90,13 +78,6 @@ dropLastByte :: BS.ByteString -> BS.ByteString
 dropLastByte (PS ptr start len) | len >= 1 = PS ptr start (len-1)
 dropLastByte  bs                           = bs
 
--- | Take n last bytes.
-revTake :: Int -> BS.ByteString -> BS.ByteString
-revTake i (PS ptr from to) = PS ptr (end-len) len
-  where
-    end = from + to
-    len = min to i
-
 {-# INLINABLE stripNS #-}
 stripNS :: ByteString -> ByteString
 stripNS = snd . splitNS
@@ -107,28 +88,4 @@ unknownChildHandler desc node = ("Unhandled child of " <> desc <> " '" <> eltNam
                                    `failHere` eltName
   where
     eltName = Xeno.name node
-
--- | Print schema errors with excerpts
-printExceptions :: BS.ByteString -> [XenoException] -> IO ()
-printExceptions i s = (BS.hPutStrLn stderr . displayException i) `mapM_` s
-
--- | Find line number of the error from ByteString index.
-lineNo :: Int -> BS.ByteString -> Int
-lineNo index bs = BS.count '\n'
-                $ BS.take index bs
-
-displayException :: BS.ByteString -> XenoException -> BS.ByteString
-displayException input (Xeno.XenoParseError i msg) =
-               "Decoding error in line " <> bshow (lineNo i input) <> ": "
-            <> msg
-            <> " at:\n"
-            <> lineContentBeforeError
-            <> lineContentAfterError
-            <> "\n" <> pointer
-  where
-    lineContentBeforeError = snd $ BS.spanEnd   eoln $ revTake 40 $ BS.take i input
-    lineContentAfterError  =       BS.takeWhile eoln $ BS.take 40 $ BS.drop i input
-    pointer                = BS.replicate (BS.length lineContentBeforeError) ' ' <> "^"
-    eoln ch                = ch /= '\n' && ch /= '\r'
-displayException _      err                        = bshow err
 
