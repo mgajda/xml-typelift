a6531c33357d022901b93e2ce4ac182d94f11bb2
Michal J. Gajda <migamake@migamake.com>
2018-11-29 00:58:43 +0100

    More units

--- test/UnitTests.hs
+++ test/UnitTests.hs
@@ -10,5 +10,10 @@ main = hspec $ do
   describe "skipDoctype" $ do
     it "strips initial doctype declaration" $ do
       skipDoctype "<?xml version=\"1.0\" encoding=\"UTF-8\"?>Hello" `shouldBe` "Hello"
+    it "strips doctype after spaces" $ do
+      skipDoctype "  \n<?xml version=\"1.0\" encoding=\"UTF-8\"?>Hello" `shouldBe` "Hello"
+    it "does not strip anything after or inside element" $ do
+      let insideElt = "<xml><?xml version=\"1.0\" encoding=\"UTF-8\"?>Hello</xml>"
+      skipDoctype  insideElt `shouldBe` insideElt
 
 
