8441bc30333df326697a708f9a691c8e02f030a7
Michal J. Gajda <mjgajda@migamake.com>
2020-01-24 23:36:02 +0100

    Flattener monad.

--- src/Schema.hs
+++ src/Schema.hs
@@ -1,33 +1,34 @@
-{-# LANGUAGE DeriveAnyClass             #-}
-{-# LANGUAGE DeriveDataTypeable         #-}
-{-# LANGUAGE DeriveGeneric              #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE Strict                     #-}
-{-# LANGUAGE ViewPatterns               #-}
+{-# LANGUAGE DeriveAnyClass      #-}
+{-# LANGUAGE DeriveDataTypeable  #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE Strict              #-}
 {-# OPTIONS_GHC -funbox-strict-fields   #-}
 -- | Simplification of XML Schema and RelaxNG schema
 module Schema where
 
-import Prelude hiding(id)
-import Control.DeepSeq
+import           Control.DeepSeq
+import           Prelude                     hiding (id)
 --import Data.ByteString.Char8 as BS
 --import Data.Set as Set
-import Data.Map
-import Data.Data
-import GHC.Generics
-import Data.Generics.Uniplate.Data()
+import           Data.Data
+import           Data.Generics.Uniplate.Data ()
+import           Data.Map
+import           GHC.Generics
 
-import FromXML(XMLString)
+import           FromXML                     (XMLString)
 
 class Default a where
   def :: a
 
+type TypeDict = Map XMLString Type
+
 -- | Top level XML Schema
 data Schema = Schema {
-    types     :: !(Map XMLString Type) -- ^ Types defined by name
-  , tops      :: ![Element]            -- ^ Possible top level elements
-  , namespace :: !XMLString
+    types     :: !TypeDict  -- ^ Types defined by name
+  , tops      :: ![Element] -- ^ Possible top level elements
+  , namespace :: !XMLString -- ^ Default namespace
   }
   deriving (Eq, Ord, Show, Generic, NFData, Data, Typeable)
 
@@ -148,4 +149,3 @@ instance NFData Schema
 instance NFData Type
 instance NFData Use
  -}
-
