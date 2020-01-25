8212c167e71c988882f6441855b7b9016f1c7105
Michal J. Gajda <mjgajda@migamake.com>
2020-01-25 08:45:23 +0100

    Run flattener in CLI.

--- src/Flatten.hs
+++ src/Flatten.hs
@@ -6,31 +6,39 @@
 {-# LANGUAGE NamedFieldPuns             #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE RecordWildCards            #-}
 {-# LANGUAGE StandaloneDeriving         #-}
-{-# LANGUAGE ViewPatterns               #-}
 -- | Here we aim to analyze the schema.
 module Flatten(flatten) where
 
-import           Control.Monad.RWS.Strict
-import qualified Data.Map.Strict                   as Map
-import           Data.Maybe                        (catMaybes)
+import           Control.Monad.Reader
+import           Control.Monad.RWS
+import qualified Data.Map.Strict       as Map
+import           Data.Maybe            (catMaybes)
 
 import           Data.Functor.Identity
-import           Data.Generics.Uniplate.Operations
-import           FromXML                           (XMLString, getStartIndex)
-import           Xeno.Types                        (XenoException (..))
+import           FromXML               (XMLString, getStartIndex)
 
 import           BaseTypes
 import           Schema
 
 data Message = Message {
-                 inType  :: XMLString
+                 inType  :: FScope
                , content :: String
                }
+
+instance Show Message where
+  show Message {..} = concat ["In ", show inType, " ", content]
 
 data FScope = ScopeElement XMLString
             | ScopeType    XMLString
             | ScopeGlobal
+
+instance Show FScope where
+  show  ScopeGlobal           = "top level"
+  show (ScopeType    ty     ) = "complex type " <> show ty
+  show (ScopeElement eltName) = "element "      <> show eltName
+
 
 -- | Flattening monad
 newtype Flattener a =
@@ -48,63 +56,63 @@ runFlattener Schema { types, tops, namespace } act =
     evalRWS loop ScopeGlobal types
   where
     loop = do
-      flattenType `mapM_` Map.keys types
-      tops'      <- flattenElt `mapM` tops
+      (unFlattener . flattenType) `mapM_` Map.keys types
+      tops'      <- (unFlattener . flattenElt) `mapM` tops
       finalTypes <- get
       return Schema { types=finalTypes, tops=tops', namespace}
       -- TODO: flatten element types
-    flattenType key = local (const $ ScopeType key) $ do
-      typ    <- gets (Map.! key)
-      typ'   <- unFlattener $ act typ
-      modify' $ Map.adjust (const typ') key
-    flattenElt elt@Element { eName, eType } = local (const $ ScopeElement eName) $ do
-      eType' <- unFlattener $ act eType
-      return  $ elt { eType=eType' }
+    flattenType :: XMLString -> Flattener ()
+    flattenType key =
+      Flattener $
+        local (const $ ScopeType key) $ do
+          typ    <- gets (Map.! key)
+          typ'   <- unFlattener $ act typ
+          modify' $ Map.adjust (const typ') key
+    flattenElt :: Element -> Flattener Element
+    flattenElt elt@Element { eName, eType } =
+      Flattener $
+        local (const $ ScopeElement eName) $ do
+          eType' <- unFlattener $ act eType
+          return  $ elt { eType=eType' }
 
 deriving instance MonadReader FScope                    Flattener
 deriving instance MonadWriter        [Message]          Flattener
 deriving instance MonadState                   TypeDict Flattener
 deriving instance MonadRWS    FScope [Message] TypeDict Flattener
 
--- * Analysis
-type SchemaError = XenoException
--- | TODO: use common code to visualize errors on the source
-
-analyze    :: Schema -> (Schema, [SchemaError])
-analyze sch = (sch, [])
-
--- * Quality check after analysis
--- | Check desired properties that should be kept after flattening
-check    :: Schema -> [SchemaError]
-check sch = mconcat [
-  --test isRestriction          "Restrictions present"
-    test isExtension            "Extensions present"
-  , test referenceToNonBaseType "Reference to non-base type"
-  ]
-  where
-    -- | Test predicate on entire Schema, and return index of first violation, if present.
-    test aTest msg = case catMaybes $ map aTest $ universeBi sch of
-                      (i:_) -> [XenoParseError i msg]
-                      []    -> []
-
-type Test t = Biplate Schema t => t -> Maybe Int
-
-
+report :: String -> Flattener ()
+report issue = Flattener $ do
+  scope <- ask
+  tell [Message scope issue]
 
 {-
--- | Tests to be performed:
-isRestriction :: Test Type
-isRestriction (Restriction {base}) = Just $ getStartIndex base
-isRestriction  _                   = Nothing
+  | Restriction {
+        base       :: !XMLString
+      , restricted :: !Restriction
+      }
+  | Extension {
+        base  :: !XMLString
+      , mixin :: !Type
+      } -- ^ Extension of complexType
+  | Complex {
+        mixed :: !Bool
+      , attrs :: ![Attr]
+      , inner :: !TyPart
+      }
  -}
+tyFlatten :: Type -> Flattener Type
+tyFlatten ty@Complex { mixed
+                     , attrs
+                     , inner } =
+  case inner of
+    Seq {} -> do
+      report $ "Seq in Complex type: " <> show  ty
+      return ty
+    Choice {} -> do
+      report $ "Choice in Complex type: " <> show  ty
+      return ty
+--tyFlatten Extension {base, mixin} = undefined
+tyFlatten ty = return ty
 
--- | Check if there are unexpanded extensions.
-isExtension :: Test Type
-isExtension   (Extension   {base}) = Just $ getStartIndex base
-isExtension    _                   = Nothing
-
--- | Check if there are unexpanded references to user-defined types.
-referenceToNonBaseType :: Test Type
-referenceToNonBaseType (Ref (isBaseHaskellType -> True)) = Nothing
---referenceToNonBaseType (Ref  aType)                      = Just $ getStartIndex aType
-referenceToNonBaseType  _                                = Nothing
+flatten :: Schema -> (Schema, [Message])
+flatten schema = runFlattener schema tyFlatten
