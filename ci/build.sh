e03030c6320c8f937167b90859e7ff2577c195e9
Michal J. Gajda <mjgajda@migamake.com>
2020-02-23 17:21:53 +0100

    Remove hpack install.

--- ci/build.sh
+++ ci/build.sh
@@ -3,31 +3,31 @@
 source ci/common.sh
 
 # Hpack
-message "Run hpack"
-cabal v1-install hpack
-hpack
+#message "Run hpack"
+#cabal v1-install hpack
+#hpack
 
 # Build it
 message "Build it"
-cabal v1-install --dependencies-only --allow-newer
-cabal v1-build   --system-ghc        --allow-newer
-cabal v1-test    --system-ghc        --allow-newer
+cabal v2-install --dependencies-only --allow-newer
+cabal v2-build   --system-ghc        --allow-newer
+cabal v2-test    --system-ghc        --allow-newer
 
 # check that CLI application is working and output is reasonable
 message "Check CLI"
-cabal v1-exec -- xml-typelift-cli --version
-cabal v1-exec -- xml-typelift-cli --help
-cabal v1-exec -- xml-typelift-cli --schema test/data/customersOrders.xsd --types > types.hs
-cabal v1-exec -- xml-typelift-cli --schema test/data/customersOrders.xsd         > parser.hs
+cabal v2-exec -- xml-typelift-cli --version
+cabal v2-exec -- xml-typelift-cli --help
+cabal v2-exec -- xml-typelift-cli --schema test/data/customersOrders.xsd --types > types.hs
+cabal v2-exec -- xml-typelift-cli --schema test/data/customersOrders.xsd         > parser.hs
 grep -z "\<data Customers.*= Customers {.*}" types.hs > /dev/null
 grep -z "\<parseTopLevelToArray " parser.hs > /dev/null
 
 message "Check generated code compiles"
 # TODO: add main action
-cabal v1-exec -- ghc types.hs  -o types.o
-cabal v1-exec -- ghc parser.hs -o parser.o
+cabal v2-exec -- ghc types.hs  -o types.o
+cabal v2-exec -- ghc parser.hs -o parser.o
 
 # check that benchmarks is working (but limit for 10 minutes only because of slow benchmarking)
 message "Benchmarks"
-timeout 30m cabal v1-bench xml-typelift
+timeout 30m cabal v2-bench xml-typelift
 
