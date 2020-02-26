ddcbcb684ea5da1237ec174e8d282b16153eba99
Dmitry Krylov <dima@krylov.ws>
2020-02-26 17:21:44 +1000

    Additional package for running

--- src/TestUtils.hs
+++ src/TestUtils.hs
@@ -20,6 +20,99 @@ import           System.IO.Temp
 import           Text.InterpolatedString.Perl6 (qc)
 
 
+data RunOptions = RunOptions
+        { verbose    :: Bool
+        , showStdout :: Bool
+        }
+
+
+instance Default RunOptions where
+    def = RunOptions { verbose = False
+                     , showStdout = False
+                     }
+
+
+data GhcTool = Runner | Compiler
+
+
+-- | Call specified process with args and print its output when it fails.
+--
+callProcess' :: RunOptions -> FilePath -> [String] -> IO ()
+callProcess' RunOptions{..} cmd args = do
+    when verbose $ putStrLn [qc|Run "{cmd}" with args: {args}|]
+    (_, pstdout, pstderr, p) <- createProcess ((proc cmd args) { std_out = if showStdout then Inherit else CreatePipe, std_err = CreatePipe })
+    waitForProcess p >>= \case
+        ExitSuccess -> do
+            unless showStdout $ whenMaybe hClose pstdout
+            whenMaybe hClose pstderr
+        ExitFailure r -> do
+            whenMaybe (dumpHandle stdout) pstdout
+            whenMaybe (dumpHandle stderr) pstderr
+            fail [qc|Running "{cmd}" "{args}" has failed with "{r}"|]
+  where
+    dumpHandle outhndl inhnd = hGetContents inhnd >>= hPutStr outhndl
+    whenMaybe a m = maybe (return ()) a m
+
+
+findGhc :: RunOptions -> GhcTool -> IO (FilePath, [String])
+findGhc RunOptions{..} ghcTool = do
+    when verbose $ do
+        let showEnv env = lookupEnv env >>= (\e -> putStrLn [qc|>>> {env} = {e}|])
+        showEnv "STACK_EXE"
+        showEnv "CABAL_SANDBOX_CONFIG"
+        showEnv "GHC_PACKAGE_PATH"
+        showEnv "HASKELL_DIST_DIR"
+        -- putStrLn "Environment: -----------"
+        -- getEnvironment >>= (mapM_ $ \(env,val) -> putStrLn [qc|{env} = "{val}"|])
+        -- putStrLn "End of environment -----"
+    stack      <- lookupEnv "STACK_EXE"
+    isOldCabal <- lookupEnv "CABAL_SANDBOX_CONFIG"
+    isNewCabal <- lookupEnv "HASKELL_DIST_DIR"
+    let res@(exe, exeArgs') | Just stackExec <- stack      = (stackExec, [tool, "--"] ++ additionalPackagesArgs)
+                            | Just _         <- isOldCabal = ("cabal", ["exec", tool, "--"])
+                            -- NB: New cabal can't find used packages, so it is need to specify it explicit
+                            | Just _         <- isNewCabal = ("cabal", ["v2-exec", tool, "--"] ++ additionalPackagesArgs)
+                            | otherwise                    = (tool, [])
+        exeArgs = case ghcTool of
+                    Compiler -> exeArgs' ++ ["-O0"]
+                    Runner   -> exeArgs'
+    when verbose $ putStrLn [qc|Use exe "{exe}", and additional arguments: {exeArgs}|]
+    return res
+  where
+    tool = case ghcTool of
+               Runner   -> "runghc"
+               Compiler -> "ghc"
+    mkAdditionalPackagesArg arg = case ghcTool of
+               Runner   -> "--ghc-arg=-package " ++ arg
+               Compiler ->            [qc|-package {arg}|]
+    additionalPackagesArgs =
+        map mkAdditionalPackagesArg additionalPackages
+    additionalPackages = ["iso8601-duration", "xeno", "xml-typelift"] -- TODO: try to get from package.yaml via Template Haskell
+
+
+passModuleToGhc :: RunOptions -> GhcTool -> FilePath -> [String] -> IO ()
+passModuleToGhc ro ghcTool moduleFilename args =
+    handle (\(e::SomeException) -> do print e >> throw e) $ do
+        (exe, exeArgs) <- findGhc ro ghcTool
+        callProcess' ro exe (exeArgs ++ moduleFilename:args)
+
+
+-- | Find ghc with cabal/stack and run it with specified arguments
+--
+compileHaskellModule :: FilePath -> [String] -> IO ()
+compileHaskellModule moduleFilename args = passModuleToGhc def Compiler moduleFilename args
+
+
+-- | Run Haskell module in specified file with arguments
+--
+runHaskellModule' :: RunOptions -> FilePath -> [String] -> IO ()
+runHaskellModule' ro moduleFilename args = passModuleToGhc ro Runner moduleFilename args
+
+
+runHaskellModule :: FilePath -> [String] -> IO ()
+runHaskellModule moduleFilename args = runHaskellModule' def moduleFilename args
+
+
 -- | Save data in temporary directory with specified filename.
 --   If failed, don't erase this temporary directory and outputs path to it for further debugging.
 --
