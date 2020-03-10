31598db154c2b3c4a535198c0e2f31d19456b220
Dmitry Krylov <dima@krylov.ws>
2020-03-10 23:02:47 +1000

    Remove unused code

--- src/TestUtils.hs
+++ src/TestUtils.hs
@@ -11,96 +11,14 @@ module TestUtils
     ) where
 
 
-import           Control.Exception
-import           Control.Monad
 import qualified Control.Monad.Catch as MC
-import           Data.Default
 import           System.Directory
-import           System.Environment
 import           System.Exit
 import           System.FilePath.Posix
 import           System.IO
 import           System.IO.Temp
-import           System.Process
 import           Text.InterpolatedString.Perl6 (qc)
 
-    {-
-
-data RunOptions = RunOptions
-        { verbose    :: Bool
-        , showStdout :: Bool
-        }
-
-
-instance Default RunOptions where
-    def = RunOptions { verbose = False
-                     , showStdout = False
-                     }
-
-
-data GhcTool = Runner | Compiler
-
-
--- | Call specified process with args and print its output when it fails.
---
-callProcess' :: RunOptions -> FilePath -> [String] -> IO ()
-callProcess' RunOptions{..} cmd args = do
-    (_, pstdout, pstderr, p) <- createProcess ((proc cmd args) { std_out = if showStdout then Inherit else CreatePipe, std_err = CreatePipe })
-    waitForProcess p >>= \case
-        ExitSuccess -> do
-            -- whenMaybe hClose pstdout
-            whenMaybe hClose pstderr
-        ExitFailure r -> do
-            whenMaybe (dumpHandle stdout) pstdout
-            whenMaybe (dumpHandle stderr) pstderr
-            fail [qc|Running "{cmd}" "{args}" has failed with "{r}"|]
-  where
-    dumpHandle outhndl inhnd = hGetContents inhnd >>= hPutStr outhndl
-    whenMaybe a m = maybe (return ()) a m
-
-
-findGhc :: RunOptions -> GhcTool -> IO (FilePath, [String])
-findGhc RunOptions{..} ghcTool = do
-    stack <- lookupEnv "STACK_EXE"
-    cabal <- lookupEnv "CABAL_SANDBOX_CONFIG"
-    when verbose $ putStrLn [qc|STACK_EXE={stack} ; CABAL_SANDBOX_CONFIG={cabal}|]
-    let res@(exe, exeArgs') | Just stackExec <- stack = (stackExec, [tool, "--"])
-                            | Just _         <- cabal = ("cabal", ["exec", tool, "--"])
-                            | otherwise               = (tool, [])
-        exeArgs = case ghcTool of
-                    Compiler -> exeArgs' ++ ["-O0"]
-                    Runner   -> exeArgs
-    when verbose $ putStrLn [qc|Use exe "{exe}", and additional arguments: {exeArgs}|]
-    return res
-  where
-    tool = case ghcTool of
-               Runner   -> "runghc"
-               Compiler ->  "ghc"
-
-
-passModuleToGhc :: RunOptions -> GhcTool -> FilePath -> [String] -> IO ()
-passModuleToGhc ro ghcTool moduleFilename args =
-    handle (\(e::SomeException) -> do print e >> throw e) $ do
-        (exe, exeArgs) <- findGhc ro ghcTool
-        callProcess' ro exe (exeArgs ++ moduleFilename:args)
-
-
--- | Find ghc with cabal/stack and run it with specified arguments
---
-compileHaskellModule :: FilePath -> [String] -> IO ()
-compileHaskellModule moduleFilename args = passModuleToGhc def Compiler moduleFilename args
-
-
--- | Run Haskell module in specified file with arguments
---
-runHaskellModule' :: RunOptions -> FilePath -> [String] -> IO ()
-runHaskellModule' ro moduleFilename args = passModuleToGhc ro Runner moduleFilename args
-
-
-runHaskellModule :: FilePath -> [String] -> IO ()
-runHaskellModule moduleFilename args = runHaskellModule' def moduleFilename args
-
--}
 
 -- | Save data in temporary directory with specified filename.
 --   If failed, don't erase this temporary directory and outputs path to it for further debugging.
